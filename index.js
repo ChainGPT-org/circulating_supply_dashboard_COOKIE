decbfc31e132f96ea7eef1f1ea789bd7b64b31a9
Ilan Rakhmanov <26630467+ceoguy@users.noreply.github.com>
2024-06-13 03:46:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -110,30 +110,6 @@ const contractAddresses = [
     type: 'Polkastarter Pad IDO Vesting',
     wallet: 'Public Round (ref: tokenomics)', 
   },
-  {
-    address: '0x538656823f1a5F82502D8A7328d7F57975fF3B72',
-    chain: 'BNB',
-    type: 'Affiliate Airdrop',
-    wallet: 'Bybit Airdrop (ref: tokenomics)', 
-  },
-  {
-    address: '0x0ECa5f003a8D2471c6089AfCB0C8fD972c4615A8',
-    chain: 'BNB',
-    type: 'Community Airdrop',
-    wallet: 'Community Airdrop (ref: tokenomics)', 
-  },
-  {
-    address: '0x45242f3520cf610abffcc0e3315c4fc6080b6154',
-    chain: 'BNB',
-    type: 'Liquidity (MS)',
-    wallet: 'Locked Liquidity (ref: tokenomics)', 
-  },
-  {
-    address: '0x4589481a385A5Fa77b2003efD4Feb34E4693254f',
-    chain: 'BNB',
-    type: 'Liquidity (MS)',
-    wallet: 'Locked Liquidity (ref: tokenomics)', 
-  },
 ];
 
 
